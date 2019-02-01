6e1f274a7a77b20de5f395edbd50d41d494d6f46
protasovse <protasovse@gmail.com>
2019-02-01 13:25:41 +0500

    week4 task with function

--- week4/index.js
+++ week4/index.js
@@ -1,28 +1,35 @@
-module.exports = function () {
-    return {
-        collection: 0,
-        /**
-         * @param {Array} collection
-         * @params {Function[]} – Функции для запроса
-         * @returns {Array}
-         */
-        query: (collection) => {
-            console.log('this', this)
-            console.log('col', this.collection)
-            this.collection = collection
-            let functions = Array.prototype.slice.call(arguments, 1)
-            for (let i in functions) {
-                void functions[i]
-            }
-            return this.collection
-        },
-
-        select: () => {
-            this.collection += 1
-        },
-
-        filterIn: () => {
-            this.collection *= 2
-        }
+/**
+ * @param {Array} collection
+ * @params {Function[]} – Функции для запроса
+ * @returns {Array}
+ */
+function query(collection) {
+    this.collection = collection;
+    let functions = Array.prototype.slice.call(arguments, 1);
+    for (let i in functions) {
+        console.log("name: ", functions[i].name);
+        functions[i](collection);
     }
+    return collection;
 }
+
+/**
+ * @params {String[]}
+ */
+function select(fields) {
+    this.collection + 1;
+}
+
+/**
+ * @param {String} property – Свойство для фильтрации
+ * @param {Array} values – Массив разрешённых значений
+ */
+function filterIn(property, values) {
+    this.collection * 2;
+}
+
+module.exports = {
+    query: query,
+    select: select,
+    filterIn: filterIn
+};
